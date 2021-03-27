1c80909a0eccf6768532e6a7e57869f4d364029b
GIlbert Kurniawan H <64368640+gilbertk27@users.noreply.github.com>
2021-03-27 20:36:51 +0700

    uupdated answer

--- soal1/soal1.sh
+++ soal1/soal1.sh
@@ -11,3 +11,15 @@ echo c_err=$(grep -cE 'ERROR' syslog.log)
 #1c
 c_user=$(grep -Po '(?<=\()(.*)(?=\))' syslog.log | sort -u)
 echo -e "\n$c_user\n"
+
+#1d
+#1d
+count_err=$( cat syslog.log | grep 'ERROR' | grep -Po '(?<=ERROR\s)(.*)(?=\s)' | sort | uniq -c)
+echo 'Error,Count' > error_message.csv
+echo "$count_err" | while read row;
+do
+error=$(echo $row | cut -d ' ' -f 2-)
+count=$(echo $row | cut -d ' ' -f 1)
+print="$error,$count"
+echo $print >> error_message.csv
+done
