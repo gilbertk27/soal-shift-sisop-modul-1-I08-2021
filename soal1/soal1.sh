c863853c894083f26dd6275107fde79be3291a1d
GIlbert Kurniawan H <64368640+gilbertk27@users.noreply.github.com>
2021-03-27 18:31:32 +0700

    updated answer

--- soal1/soal1.sh
+++ soal1/soal1.sh
@@ -3,19 +3,7 @@
 #1a
 grep -oE "(INFO.*)|(ERROR.*)" syslog.log
 
-input='syslog.log'
-error=$(cat $input | grep 'ERROR')
-info=$(cat $input | grep 'INFO')
-#echo -e "$error\n"
-#echo -e "$info\n"
-c_error=$(grep -c 'ERROR' $input)
-c_info=$(grep -c 'INFO' $input)
-#echo -e "$c_error\n"
-#echo -e "$c_info\n"
-
 #1b
-msg=$(echo "$error" | grep '(?<=ERROR\s)(.*)(?= )')
-c_msg=$(echo "$msg" | sort -s | uniq -c )
-#echo -e "$msg\n"
-#echo -e "$c_msg\n"
+grep -oE 'ERROR.*' syslog.log
+echo c_err = $(grep -cE 'ERROR' syslog.log) 
 
