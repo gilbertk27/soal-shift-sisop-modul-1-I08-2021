8e398fc11a62d320fe5359472a7d2bae6e8dff18
GIlbert Kurniawan H <64368640+gilbertk27@users.noreply.github.com>
2021-03-27 19:02:51 +0700

    updated answer

--- soal1/soal1.sh
+++ soal1/soal1.sh
@@ -3,7 +3,11 @@
 #1a
 grep -oE "(INFO.*)|(ERROR.*)" syslog.log
 
+
 #1b
 grep -oE 'ERROR.*' syslog.log
-echo c_err = $(grep -cE 'ERROR' syslog.log) 
+echo c_err=$(grep -cE 'ERROR' syslog.log) 
 
+#1c
+c_user=$(grep -Po '(?<=\()(.*)(?=\))' syslog.log | sort -u)
+echo -e "\n$c_user\n"
