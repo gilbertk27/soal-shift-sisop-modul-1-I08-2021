ec3010c5a63c259fd4fe6913920bdce730cae8d9
GIlbert Kurniawan H <64368640+gilbertk27@users.noreply.github.com>
2021-04-01 07:03:46 +0700

    update answer

--- soal3/soal3a.sh
+++ soal3/soal3a.sh
@@ -1,1 +1,11 @@
- 
+#!/bin/bash
+
+for ((a=1; a<=24; a=+1))
+do
+	if [ $a -lt 10 ]
+	then
+		wget -O Koleksi_0$a https://loremflickr.com/320/240/kitten &>> Foto.log
+	else
+		wget -O Koleksi_$a https://loremflickr.com/320/240/kitten &>> Foto.log
+	fi
+done
--- soal3/soal3d.sh
+++ soal3/soal3d.sh
@@ -1,1 +1,5 @@
- 
+ #!/bin/bash
+
+passw=`date +'%m%d%Y'`
+zip -r -P $passw Koleksi.zip .
+
