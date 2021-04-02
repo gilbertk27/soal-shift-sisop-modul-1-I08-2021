60582f2f12d9d92316828dc34a1d0a23ff3de09b
GIlbert Kurniawan H <64368640+gilbertk27@users.noreply.github.com>
2021-04-02 07:15:56 +0700

    revised 1a&1e

--- soal1/soal1.sh
+++ soal1/soal1.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #1a
-grep -oE "(INFO.*)|(ERROR.*)" syslog.log
-
+p_error=$(cat syslog.log | grep -Po '(?<=: )(.*)(?<=\))' | sort -u)
+echo -e "$p_error \n"
 
 #1b
 grep -oE 'ERROR.*' syslog.log
@@ -13,7 +13,6 @@ c_user=$(grep -Po '(?<=\()(.*)(?=\))' syslog.log | sort -u)
 echo -e "\n$c_user\n"
 
 #1d
-#1d
 count_err=$( cat syslog.log | grep 'ERROR' | grep -Po '(?<=ERROR\s)(.*)(?=\s)' | sort | uniq -c)
 echo 'Error,Count' > error_message.csv
 echo "$count_err" | while read row;
@@ -22,4 +21,13 @@ error=$(echo $row | cut -d ' ' -f 2-)
 count=$(echo $row | cut -d ' ' -f 1)
 print="$error,$count"
 echo $print >> error_message.csv
+done
+
+#1e
+echo 'Username,INFO,ERROR' > user_statistic.csv
+for i in $c_user
+do
+err_c=$(cat syslog.log | grep 'ERROR' | grep -c $i)
+info_c=$(cat syslog.log | grep 'INFO' | grep -c $i)
+echo "$i,$info_c,$err_c" >> user_statistic.csv
 done
